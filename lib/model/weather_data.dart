d9a752f8fabd1b6ce09964c9f280db297fe3503f
Kaito Kudou <kaitokudou2468110921@gmail.com>
2023-12-12 11:54:48 +0900

    [fix]FormatExceptionをthrowするように修正

--- lib/model/weather_data.dart
+++ lib/model/weather_data.dart
@@ -11,12 +11,19 @@ class WeatherResponse {
 
   factory WeatherResponse.fromJson(String jsonString) {
     final decoded = jsonDecode(jsonString) as Map<String, dynamic>;
+    final weatherCondition =
+        WeatherCondition.from(decoded['weather_condition'].toString());
+    final maxTemperature = int.parse(
+      decoded['max_temperature'].toString(),
+    );
+    final minTemperature = int.parse(
+      decoded['min_temperature'].toString(),
+    );
 
     return WeatherResponse(
-      weatherCondition:
-          WeatherCondition.from(decoded['weather_condition'] as String),
-      maxTemperature: decoded['max_temperature'] as int,
-      minTemperature: decoded['min_temperature'] as int,
+      weatherCondition: weatherCondition,
+      maxTemperature: maxTemperature,
+      minTemperature: minTemperature,
     );
   }
 
