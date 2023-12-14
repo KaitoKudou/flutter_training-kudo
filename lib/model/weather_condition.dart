93a74bb3d22bb79b804ebdabb0ad720de6b9d292
Kaito Kudou <kaitokudou2468110921@gmail.com>
2023-12-14 15:06:27 +0900

    [remove]factoryメソッドを削除

--- lib/model/weather_condition.dart
+++ lib/model/weather_condition.dart
@@ -2,11 +2,4 @@ enum WeatherCondition {
   sunny,
   cloudy,
   rainy;
-
-  factory WeatherCondition.from(String path) {
-    return WeatherCondition.values.singleWhere(
-      (condition) => condition.name == path,
-      orElse: () => throw const FormatException('不適切なデータを受け取りました'),
-    );
-  }
 }
