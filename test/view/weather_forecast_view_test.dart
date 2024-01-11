213c3c97404f74d0b5989ebd838d799c03bf9c48
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-01-11 10:49:59 +0900

    [fix]天気予報画面に画像と気温が表示されていることを確認するテストを追加

--- test/view/weather_forecast_view_test.dart
+++ test/view/weather_forecast_view_test.dart
@@ -53,4 +53,173 @@ void main() {
     final actual = find.bySemanticsLabel('sunny image');
     expect(actual, findsOneWidget);
   });
+
+  testWidgets('天気予報画面に曇りの画像が表示される', (widgetTester) async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
+
+    addTearDown(() => binding.setSurfaceSize(null));
+
+    // Arrange
+    await widgetTester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          yumemiWeatherProvider.overrideWithValue(mockYumemiWeather),
+        ],
+        child: const MaterialApp(
+          home: WeatherForecastView(),
+        ),
+      ),
+    );
+    const response = '''
+      {
+        "weather_condition":"cloudy",
+        "max_temperature":25,
+        "min_temperature":7
+       }
+      ''';
+    when(mockYumemiWeather.fetchWeather(any)).thenReturn(response);
+
+    // Act
+    await widgetTester.tap(find.byKey(CommonTextButton.reloadButton));
+    await widgetTester.pump();
+
+    // Assert
+    final actual = find.bySemanticsLabel('cloudy image');
+    expect(actual, findsOneWidget);
+  });
+
+  testWidgets('天気予報画面に雨の画像が表示される', (widgetTester) async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
+
+    addTearDown(() => binding.setSurfaceSize(null));
+
+    // Arrange
+    await widgetTester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          yumemiWeatherProvider.overrideWithValue(mockYumemiWeather),
+        ],
+        child: const MaterialApp(
+          home: WeatherForecastView(),
+        ),
+      ),
+    );
+    const response = '''
+      {
+        "weather_condition":"rainy",
+        "max_temperature":25,
+        "min_temperature":7
+       }
+      ''';
+    when(mockYumemiWeather.fetchWeather(any)).thenReturn(response);
+
+    // Act
+    await widgetTester.tap(find.byKey(CommonTextButton.reloadButton));
+    await widgetTester.pump();
+
+    // Assert
+    final actual = find.bySemanticsLabel('rainy image');
+    expect(actual, findsOneWidget);
+  });
+
+  testWidgets('天気予報画面に最高気温が表示される', (widgetTester) async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
+
+    addTearDown(() => binding.setSurfaceSize(null));
+
+    // Arrange
+    await widgetTester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          yumemiWeatherProvider.overrideWithValue(mockYumemiWeather),
+        ],
+        child: const MaterialApp(
+          home: WeatherForecastView(),
+        ),
+      ),
+    );
+    const response = '''
+      {
+        "weather_condition":"sunny",
+        "max_temperature":25,
+        "min_temperature":7
+       }
+      ''';
+    when(mockYumemiWeather.fetchWeather(any)).thenReturn(response);
+
+    // Act
+    await widgetTester.tap(find.byKey(CommonTextButton.reloadButton));
+    await widgetTester.pump();
+
+    // Assert
+    final actual = find.text('25℃');
+    expect(actual, findsOneWidget);
+  });
+
+  testWidgets('天気予報画面に最低気温が表示される', (widgetTester) async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
+
+    addTearDown(() => binding.setSurfaceSize(null));
+
+    // Arrange
+    await widgetTester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          yumemiWeatherProvider.overrideWithValue(mockYumemiWeather),
+        ],
+        child: const MaterialApp(
+          home: WeatherForecastView(),
+        ),
+      ),
+    );
+    const response = '''
+      {
+        "weather_condition":"sunny",
+        "max_temperature":25,
+        "min_temperature":7
+       }
+      ''';
+    when(mockYumemiWeather.fetchWeather(any)).thenReturn(response);
+
+    // Act
+    await widgetTester.tap(find.byKey(CommonTextButton.reloadButton));
+    await widgetTester.pump();
+
+    // Assert
+    final actual = find.text('7℃');
+    expect(actual, findsOneWidget);
+  });
+
+  testWidgets('天気予報画面にダイアログが表示され、特定のメッセージが表示されること', (widgetTester) async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
+
+    addTearDown(() => binding.setSurfaceSize(null));
+
+    // Arrange
+    await widgetTester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          yumemiWeatherProvider.overrideWithValue(mockYumemiWeather),
+        ],
+        child: const MaterialApp(
+          home: WeatherForecastView(),
+        ),
+      ),
+    );
+    when(mockYumemiWeather.fetchWeather(any))
+        .thenThrow(YumemiWeatherError.unknown);
+
+    // Act
+    await widgetTester.tap(find.byKey(CommonTextButton.reloadButton));
+    await widgetTester.pump();
+
+    // Assert
+    final actual = find.widgetWithText(AlertDialog, '不明なエラーです');
+    expect(actual, findsOneWidget);
+  });
 }
