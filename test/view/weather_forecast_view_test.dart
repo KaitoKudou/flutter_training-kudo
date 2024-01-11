a3911ddbe5d4bc5db8e618fee363827764330017
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-01-11 12:45:31 +0900

    [fix]初期状態とクローズボタン押下時のテストを追加、リファクタリング

--- test/view/weather_forecast_view_test.dart
+++ test/view/weather_forecast_view_test.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/main.dart';
 import 'package:flutter_training/provider/yumemi_weather_provider.dart';
 import 'package:flutter_training/view/component/common_text_button.dart';
+import 'package:flutter_training/view/launch_view.dart';
 import 'package:flutter_training/view/weather_forecast_view.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -13,6 +15,20 @@ import 'weather_forecast_view_test.mocks.dart';
 @GenerateNiceMocks([MockSpec<YumemiWeather>()])
 void main() {
   late MockYumemiWeather mockYumemiWeather;
+  const defaultResponse = '''
+      {
+        "weather_condition":"sunny",
+        "max_temperature":25,
+        "min_temperature":7
+       }
+      ''';
+
+  Future<void> initializeDeviceSurfaceSize() async {
+    final binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
+
+    addTearDown(() => binding.setSurfaceSize(null));
+  }
 
   setUp(() {
     // Common Arrange
@@ -20,10 +36,7 @@ void main() {
   });
 
   testWidgets('天気予報画面に晴れの画像が表示される', (widgetTester) async {
-    final binding = TestWidgetsFlutterBinding.ensureInitialized();
-    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
-
-    addTearDown(() => binding.setSurfaceSize(null));
+    await initializeDeviceSurfaceSize();
 
     // Arrange
     await widgetTester.pumpWidget(
@@ -36,14 +49,7 @@ void main() {
         ),
       ),
     );
-    const response = '''
-      {
-        "weather_condition":"sunny",
-        "max_temperature":25,
-        "min_temperature":7
-       }
-      ''';
-    when(mockYumemiWeather.fetchWeather(any)).thenReturn(response);
+    when(mockYumemiWeather.fetchWeather(any)).thenReturn(defaultResponse);
 
     // Act
     await widgetTester.tap(find.byKey(CommonTextButton.reloadButton));
@@ -55,10 +61,7 @@ void main() {
   });
 
   testWidgets('天気予報画面に曇りの画像が表示される', (widgetTester) async {
-    final binding = TestWidgetsFlutterBinding.ensureInitialized();
-    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
-
-    addTearDown(() => binding.setSurfaceSize(null));
+    await initializeDeviceSurfaceSize();
 
     // Arrange
     await widgetTester.pumpWidget(
@@ -90,10 +93,7 @@ void main() {
   });
 
   testWidgets('天気予報画面に雨の画像が表示される', (widgetTester) async {
-    final binding = TestWidgetsFlutterBinding.ensureInitialized();
-    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
-
-    addTearDown(() => binding.setSurfaceSize(null));
+    await initializeDeviceSurfaceSize();
 
     // Arrange
     await widgetTester.pumpWidget(
@@ -125,10 +125,7 @@ void main() {
   });
 
   testWidgets('天気予報画面に最高気温が表示される', (widgetTester) async {
-    final binding = TestWidgetsFlutterBinding.ensureInitialized();
-    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
-
-    addTearDown(() => binding.setSurfaceSize(null));
+    await initializeDeviceSurfaceSize();
 
     // Arrange
     await widgetTester.pumpWidget(
@@ -141,14 +138,7 @@ void main() {
         ),
       ),
     );
-    const response = '''
-      {
-        "weather_condition":"sunny",
-        "max_temperature":25,
-        "min_temperature":7
-       }
-      ''';
-    when(mockYumemiWeather.fetchWeather(any)).thenReturn(response);
+    when(mockYumemiWeather.fetchWeather(any)).thenReturn(defaultResponse);
 
     // Act
     await widgetTester.tap(find.byKey(CommonTextButton.reloadButton));
@@ -160,10 +150,7 @@ void main() {
   });
 
   testWidgets('天気予報画面に最低気温が表示される', (widgetTester) async {
-    final binding = TestWidgetsFlutterBinding.ensureInitialized();
-    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
-
-    addTearDown(() => binding.setSurfaceSize(null));
+    await initializeDeviceSurfaceSize();
 
     // Arrange
     await widgetTester.pumpWidget(
@@ -176,14 +163,7 @@ void main() {
         ),
       ),
     );
-    const response = '''
-      {
-        "weather_condition":"sunny",
-        "max_temperature":25,
-        "min_temperature":7
-       }
-      ''';
-    when(mockYumemiWeather.fetchWeather(any)).thenReturn(response);
+    when(mockYumemiWeather.fetchWeather(any)).thenReturn(defaultResponse);
 
     // Act
     await widgetTester.tap(find.byKey(CommonTextButton.reloadButton));
@@ -195,10 +175,7 @@ void main() {
   });
 
   testWidgets('天気予報画面にダイアログが表示され、特定のメッセージが表示されること', (widgetTester) async {
-    final binding = TestWidgetsFlutterBinding.ensureInitialized();
-    await binding.setSurfaceSize(const Size(1179, 2556)); // iPhone15のサイズ
-
-    addTearDown(() => binding.setSurfaceSize(null));
+    await initializeDeviceSurfaceSize();
 
     // Arrange
     await widgetTester.pumpWidget(
@@ -221,5 +198,64 @@ void main() {
     // Assert
     final actual = find.widgetWithText(AlertDialog, '不明なエラーです');
     expect(actual, findsOneWidget);
+  });
+
+  testWidgets('初期状態でプレースホルダー画像が表示される', (widgetTester) async {
+    await initializeDeviceSurfaceSize();
+
+    // Arrange
+    await widgetTester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: WeatherForecastView(),
+        ),
+      ),
+    );
+
+    // Assert
+    final actual = find.byType(Placeholder);
+    expect(actual, findsOneWidget);
+  });
+
+  testWidgets('初期状態でデフォルトの最高気温と最低気温が表示される', (widgetTester) async {
+    await initializeDeviceSurfaceSize();
+
+    // Arrange
+    await widgetTester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: WeatherForecastView(),
+        ),
+      ),
+    );
+
+    // Assert
+    final actual = find.text('**℃');
+    expect(actual, findsNWidgets(2));
+  });
+
+  testWidgets('クローズボタン押下で画面を閉じる', (widgetTester) async {
+    await initializeDeviceSurfaceSize();
+
+    await widgetTester.runAsync(() async {
+      // Arrange
+      await widgetTester.pumpWidget(
+        const ProviderScope(
+          child: MaterialApp(
+            home: MainApp(),
+          ),
+        ),
+      );
+      await Future<void>.delayed(const Duration(milliseconds: 500));
+      await widgetTester.pumpAndSettle();
+
+      // Act
+      await widgetTester.tap(find.byKey(CommonTextButton.closeButton));
+      await widgetTester.pumpAndSettle();
+
+      // Assert
+      final actual = find.byType(LaunchView);
+      expect(actual, findsOneWidget);
+    });
   });
 }
