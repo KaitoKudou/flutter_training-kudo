f2ad5934475f6b445ae694ee1f9ec2416117c36e
Kaito Kudou <kaitokudou2468110921@gmail.com>
2024-01-17 12:49:28 +0900

    [fix]weatherDataStateNotifierProviderを使って表示するように修正

--- lib/view/component/common_temperature_text.dart
+++ lib/view/component/common_temperature_text.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_training/notifier/weather_forecast_view_state.dart';
+import 'package:flutter_training/provider/weather_data_state_notifier.dart';
 
 class CommonTemperatureText extends ConsumerWidget {
   const CommonTemperatureText({
@@ -10,20 +10,15 @@ class CommonTemperatureText extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final textTheme = Theme.of(context).textTheme;
-    final minTemperature = ref.watch(
-      weatherForecastViewStateProvider
-          .select((weatherData) => weatherData?.minTemperature),
-    );
-    final maxTemperature = ref.watch(
-      weatherForecastViewStateProvider
-          .select((weatherData) => weatherData?.maxTemperature),
-    );
+    final weatherData = ref.watch(weatherDataStateNotifierProvider);
 
     return Row(
       children: [
         Expanded(
           child: Text(
-            minTemperature == null ? '**℃' : '$minTemperature℃',
+            weatherData?.minTemperature == null
+                ? '**℃'
+                : '${weatherData!.minTemperature}℃',
             style: textTheme.labelLarge!.copyWith(
               color: Colors.blue,
             ),
@@ -32,7 +27,9 @@ class CommonTemperatureText extends ConsumerWidget {
         ),
         Expanded(
           child: Text(
-            maxTemperature == null ? '**℃' : '$maxTemperature℃',
+            weatherData?.maxTemperature == null
+                ? '**℃'
+                : '${weatherData!.maxTemperature}℃',
             style: textTheme.labelLarge!.copyWith(
               color: Colors.red,
             ),
